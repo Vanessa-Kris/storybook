2b2a270fd7a35277d8379bd242022f96cd0a9359
Minion <nessakristo@gmail.com>
2023-05-18 23:30:19 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import "./App.css";
 import Posts from "./components/Posts/Posts";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   return (
-    <div className="main-container">
-      <h1 className="main-heading">Blog App using React Js</h1>
-      <Posts />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1 className="main-heading">Blog App using React Js</h1>
+        <Posts />
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Post.css";
+import Container from "@mui/material/Container";
+
 const Post = ({ post: { title, body, imgUrl, author }, index }) => {
   return (
-    <div className="post-container">
+    <Container>
       <h1 className="heading">{title}</h1>
       <img className="image" src={imgUrl} alt="post" />
       <p>{body}</p>
       <div className="info">
         <h4>Written by: {author}</h4>
       </div>
-    </div>
+    </Container>
   );
 };
 
